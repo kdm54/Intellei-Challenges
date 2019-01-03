a712dd33e04729e5b198d70d35dc9441e11f84cf
Kyra McDonald <kdm54@duke.edu>
2019-01-03 17:48:48 -0500

    fixing image URL deployed

--- serverless-dogs/handler.js
+++ serverless-dogs/handler.js
@@ -1,10 +1,29 @@
 'use strict';
-//const dog = require("'./dogImageGenerator.js'")
+const dog = require('./getRandomImageOfSubBreed(hound-basset)')
 //const dictionary = require("./dictionary")
+const axios = require('axios');
 module.exports.execute = (event, context, callback) => {
   const response = {
     statusCode: 200,
-    body: JSON.stringify('https://images.dog.ceo/breeds/hound-basset/'),
+    body: dog(),
   };
 callback(null, response);
 };
+
+async function getRandomImageOfSubBreed(subBreed) {
+  const response = await axios.get('https://dog.ceo/api/breeds/list/all');
+      var dog = "";
+      var allBreeds = response.data.message;
+      //for each breed of dog:
+      for (var breed in allBreeds){
+        //if the index of that specific breed has a subBreed:
+        //(it's subBreed count must be greater than 0 for it to have a subBreed)
+        if (allBreeds[breed].indexOf(subBreed) >= 0){
+          dog = breed;
+          //got URL from the dog API random generator section
+          var random = await axios.get('https://dog.ceo/api/breed/' + dog + '/' + subBreed + '/images/random');
+          //returns value of random
+          return random.data.message;
+        }
+      }
+}
